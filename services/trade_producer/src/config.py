d9f6b9c3ef0a2bf04dac540c353b5214fd9fd67f
Job Quiroz <jobquiroz@hotmail.com>
2024-09-29 20:12:55 -0600

    feat: Trade producer can ingest historical trade_data_source
    > Added additional prameters to our config
    > Created 2 more .env config files
    > Added extra commands in our Mafiles

--- services/trade_producer/src/config.py
+++ services/trade_producer/src/config.py
@@ -1,11 +1,14 @@
-
+from typing import Optional
 from pydantic_settings import BaseSettings
 
 class AppConfig(BaseSettings):
     kafka_broker_address: str
     kafka_topic: str
     product_id: str
-    
+
+    live_or_historical: Optional[str] = None
+    last_n_days: Optional[int] = None
+
     # One way:
     class Config:
         env_file = ".env"
